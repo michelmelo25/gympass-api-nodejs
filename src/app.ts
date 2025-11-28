46df88756b1ea069b58e8ba80bda94c47e7ca91a
Michel Melo <michelmelosj25@hotmail.com>
2025-11-28 01:17:31 -0300

    configurações do prisma e docker com postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,28 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
-export const app = fastify();
+// Carregue a URL do DB do processo (geralmente via .env)
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+// Crie o pool de conexão do driver e o adaptador
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+export const app = fastify()
+
+const prisma = new PrismaClient({
+  adapter,
+})
+
+prisma.user.create({
+  data: {
+    name: 'Mihel',
+    email: 'michel@mail.com',
+  },
+})
