367c43288d29352ee1a39302d53cda4a2959b358
Michel Melo <michelmelosj25@hotmail.com>
2025-12-04 15:16:41 -0300

    controllers e testes E2E - Repositorio Prisma de Academias

---
+++ src/repositories/prisma/prisma.gyms.repository.ts
@@ -0,0 +1,49 @@
+import { GymCreateInput } from 'generated/prisma/models'
+import { FindhManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+import { Gym } from 'generated/prisma/client'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findhManyNearby({ latitude, longitude }: FindhManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms 
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * 
+    cos( radians( latitude ) ) * cos( radians( longitude ) - 
+    radians(${longitude}) ) + sin( radians(${latitude}) ) * 
+    sin( radians( latitude ) ) ) ) <= 10`
+
+    return gyms
+  }
+
+  async create(data: GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}
