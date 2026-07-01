3a2205ff0e3e1a807a5998089add6c313263795a
Michel Melo <michelmelosj25@hotmail.com>
2026-07-01 15:01:09 -0300

    controllers de academias

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,25 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import z from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gym-use-case'
+
+export async function nearby(request: FastifyRequest, replay: FastifyReply) {
+  const nearbyGymQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = nearbyGymQuerySchema.parse(request.body)
+
+  const fetchNearvyGymUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearvyGymUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return replay.status(200).send({ gyms })
+}
