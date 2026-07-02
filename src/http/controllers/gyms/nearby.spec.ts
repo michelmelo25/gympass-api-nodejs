33c8f21c266855010a9ae5fbe8c2adb60e751fd7
Michel Melo <michelmelosj25@hotmail.com>
2026-07-01 21:52:48 -0300

    testes e2e controllers academia

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,57 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { CreateAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Nearby Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('shoul be able to list nearby gyms', async () => {
+    const { token } = await CreateAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'The Javaa',
+        phone: '88 9 83745695',
+        latitude: -3.7257092,
+        longitude: -38.567875,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'The Typeeee',
+        phone: '88 9 83745183',
+        latitude: -4.9699222,
+        longitude: -39.0158078,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -3.7257092,
+        longitude: -38.567875,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    // expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toContainEqual(
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    )
+  })
+})
---
+++ src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,56 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { CreateAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search Gym (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('shoul be able search gym by title', async () => {
+    const { token } = await CreateAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'JavaScript Gym',
+        description: 'The Javaa',
+        phone: '88 9 83745695',
+        latitude: -3.7257092,
+        longitude: -38.567875,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'TypeScript Gym',
+        description: 'The Typeeee',
+        phone: '88 9 83745183',
+        latitude: -3.7257092,
+        longitude: -38.567875,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'JavaScript',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    // expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toContainEqual(
+      expect.objectContaining({
+        title: 'JavaScript Gym',
+      }),
+    )
+  })
+})
